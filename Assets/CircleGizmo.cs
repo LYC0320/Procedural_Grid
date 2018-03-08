1fc138cbd1df835a58df351b28c7966cc54e065e
LYC0320 <blackfield0320@gmail.com>
2018-03-08 18:06:09 +0800

    CubeSphere_1

---
+++ Assets/CircleGizmo.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleGizmo : MonoBehaviour {
+
+    public int resolution = 10;
+
+	private void OnDrawGizmosSelected()
+    {
+        float step = 2f / resolution;
+
+        for (int i = 0; i <= resolution; i++)
+        {
+            ShowPoints(i * step - 1f, -1f);
+            ShowPoints(i * step - 1f, 1f);
+        }
+
+        for (int i = 0; i <= resolution; i++)
+        {
+            ShowPoints(-1f, i * step - 1f);
+            ShowPoints(1f, i * step - 1f);
+        }
+
+    }
+
+    private void ShowPoints(float x, float y)
+    {
+        Vector2 square = new Vector2(x, y);
+
+        Vector2 circle = square.normalized;
+        Vector2 origin = Vector2.zero;
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawSphere(square, 0.025f);
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawSphere(circle, 0.025f);
+        Gizmos.DrawLine(origin, circle);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(circle, square);
+    }
+}
